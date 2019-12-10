7d82136aa14ace4e498732b90b24fbaa388633dc
Ian Walter <public@iankwalter.com>
2019-12-10 12:53:10 -0500

    Moving source before plugin install

--- install.sh
+++ install.sh
@@ -8,11 +8,11 @@ fi
 # Copy the configuration file to the home directory.
 cp .tmux.conf ~/
 
-# Install plugins.
-~/.tmux/plugins/tpm/bin/install_plugins
-
 # Tell tmux to source the config file.
 tmux source ~/.tmux.conf
+
+# Install plugins.
+~/.tmux/plugins/tpm/bin/install_plugins
 
 if [[ $? == 0 ]]; then
   printf '\n✅ Installed tmux configuration successfully.\n'
